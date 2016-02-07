2cfe9baeef921f246d278bd559d03194bf004241
Joseph Chiang <josephchiang28@gmail.com>
2016-02-07 03:17:13 -0800

    Users can now register with confirmation email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'assassinxgame.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               ENV["ASSASSINXGAME_MAIL_DOMAIN"],
+      user_name:            ENV["ASSASSINXGAME_MAIL_USER_NAME"],
+      password:             ENV["ASSASSINXGAME_MAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 end
